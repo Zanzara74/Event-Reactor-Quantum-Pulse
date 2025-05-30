751def8f0a032e0551a6051ce05ec64cedab5785
Zanzara74 <fr3dpaper@gmail.com>
2025-05-30 18:32:19 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,6 @@
+import os
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+FMP_API_KEY = os.getenv("FMP_API_KEY")
 # Configuration parameters placeholder
